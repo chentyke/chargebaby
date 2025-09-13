7eaefdfd6a6869b92ba3f08be8085e23cf089267
chentyke <chetyke@gmail.com>
2025-09-13 20:21:14 +0800

    修复能量体积比解析

--- src/app/[model]/detail/page.tsx
+++ src/app/[model]/detail/page.tsx
@@ -39,7 +39,7 @@ export default async function DetailDataPage({ params }: PageProps) {
             <DataItem label="重量" value={`${detailData.weight}g`} />
             <DataItem label="体积" value={`${detailData.volume}cm³`} />
             <DataItem label="能量重量比" value={`${detailData.capacityWeightRatio} Wh/g`} />
-            <DataItem label="能量体积比" value={`${detailData.energyVolumeRatio || detailData.capacityVolumeRatio} Wh/cm³`} />
+            <DataItem label="能量体积比" value={`${detailData.capacityVolumeRatio} Wh/cm³`} />
           </DataSection>
 
           {/* 电池容量 */}
